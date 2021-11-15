60ab806108e8d89aebd32243b767773a513741e7
Arjun Singh <arjunmann73@gmail.com>
2021-11-15 19:39:08 +0800

    Added tesseract OCR functionality

--- main.py
+++ main.py
@@ -4,24 +4,12 @@ from numpy import asarray
 from otsu import OtsuThresholder
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_local
+import pytesseract
+import logging
+logging.basicConfig(filename="logs/logs2.log", filemode="w", level=logging.INFO, format='%(message)s')
 
-if __name__ == "__main__":
-    image = Image.open("images/image1.png")
-    imageOriginal = asarray(image)
-    
-    image = ImageOps.grayscale(image)
-    image = asarray(image)
 
-    Otsu = OtsuThresholder(image)
-    threshold = Otsu.Otsu()
-
-    block_size = 35
-    adaptive_thresh = threshold_local(image, block_size, offset=10)
-    binary_adaptive = image > adaptive_thresh
-
-    binary = image > threshold
-    binary = binary.astype(int)
-
+def plotImages(image, binary):
     fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
     ax = axes.ravel()
     ax[0] = plt.subplot(1, 3, 1, adjustable='box')
@@ -41,8 +29,45 @@ if __name__ == "__main__":
     ax[2].axis('off')
 
     plt.show()
-   
-    #binaryImage.show()
+
+if __name__ == "__main__":
+    image = Image.open("images/image2.png")
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    imageOriginal = asarray(image)
+    logging.info("Basic image:")
+    text = pytesseract.image_to_string(image)
+    logging.info(text)
+    
+    image = ImageOps.grayscale(image)
+    image = asarray(image)
+    logging.info("Grayscale image image:")
+    text = pytesseract.image_to_string(image)
+    logging.info(text)
+
+    block_size = 35
+    adaptive_thresh = threshold_local(image, block_size, offset=10)
+    binary_adaptive = image > adaptive_thresh
+
+    logging.info("Addaptive threshold binarization image:")
+    text = pytesseract.image_to_string(binary_adaptive)
+    logging.info(text)
+
+    Otsu = OtsuThresholder(image)
+    threshold = Otsu.Otsu()
+    binary = image > threshold
+    binary = binary.astype(int)
+
+    logging.info("Otsu threshold binarization image:")
+    text = pytesseract.image_to_string(binary)
+    logging.info(text)
+
+    plotImages(image, binary)
+    plotImages(image, binary_adaptive)
+
+
+
+
+    
     
 
     
